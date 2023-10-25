cddd9a439a4627fdfb2f58edf7c1f182c53d5fcd
AlsonC08 <148827142+AlsonC08@users.noreply.github.com>
2023-10-25 07:51:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,30 @@ window.addEventListener('load', function() {
         }
     }, 8000); // 8 seconds in milliseconds
 });
+
+
+function displayAd() {
+    var adContainer = document.createElement("div");
+    adContainer.id = "ad-container"; // Assign a unique ID to the ad container if needed
+    document.body.appendChild(adContainer);
+
+    var adScript = document.createElement("script");
+    adScript.async = true;
+    adScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+    adScript.crossorigin = "anonymous";
+    document.body.appendChild(adScript);
+
+    var adElement = document.createElement("ins");
+    adElement.className = "adsbygoogle";
+    adElement.style.display = "block";
+    adElement.setAttribute("data-ad-format", "fluid");
+    adElement.setAttribute("data-ad-layout-key", "-ha+2t+5l-5k-6a");
+    adElement.setAttribute("data-ad-client", "ca-pub-3160553381689260");
+    adElement.setAttribute("data-ad-slot", "1359848004");
+
+    // Add the ad element to the ad container
+    adContainer.appendChild(adElement);
+
+    // Include the AdSense JavaScript asynchronously
+    (adsbygoogle = window.adsbygoogle || []).push({});
+}
